45c4ee34f3ae5a6dba5925863fcde562df8d56b7
Bhavya1999rs <www.brsonawane3964@gmail.com>
2023-10-19 17:46:39 +0530

    entity class commit

--- src/main/java/com/cg/Entity/Department.java
+++ src/main/java/com/cg/Entity/Department.java
@@ -5,6 +5,46 @@ package com.cg.Entity;
 
 
 public class Department {
+	private long departmentId;
+	private String departmentName;
+	private String departmentAddress;
+	
+	public Department() {
+	}
+	
+	
+	public Department(long departmentId, String departmentName, String departmentAddress) {
+		super();
+		this.departmentId = departmentId;
+		this.departmentName = departmentName;
+		this.departmentAddress = departmentAddress;
+	}
+	public long getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(long departmentId) {
+		this.departmentId = departmentId;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public String getDepartmentAddress() {
+		return departmentAddress;
+	}
+	public void setDepartmentAddress(String departmentAddress) {
+		this.departmentAddress = departmentAddress;
+	}
+	@Override
+	public String toString() {
+		return "Department [departmentId=" + departmentId + ", departmentName=" + departmentName
+				+ ", departmentAddress=" + departmentAddress + "]";
+	}
+	
+	
+	
 
 	
 
