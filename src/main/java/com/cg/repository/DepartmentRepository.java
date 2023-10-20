3ad4d280f2ea0596fe2acf7ec0a023406bfbcd16
Bhavya1999rs <148413959+Bhavya1999rs@users.noreply.github.com>
2023-10-20 10:32:29 +0530

    Update DepartmentRepository.java

--- src/main/java/com/cg/repository/DepartmentRepository.java
+++ src/main/java/com/cg/repository/DepartmentRepository.java
@@ -1,7 +1,12 @@
 package com.cg.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DepartmentRepository {
+import com.cg.Entity.Department;
 
+
+@org.springframework.stereotype.Repository
+
+public interface DepartmentRepository extends JpaRepository<Department, String> {
 }
