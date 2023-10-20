7fe15dac8191fa284f10c08132c9e5ffe7b8754d
Bhavya1999rs <148413959+Bhavya1999rs@users.noreply.github.com>
2023-10-20 10:33:19 +0530

    Update DepartmentDao.java

--- src/main/java/com/cg/Dao/DepartmentDao.java
+++ src/main/java/com/cg/Dao/DepartmentDao.java
@@ -1,5 +1,19 @@
 package com.cg.Dao;
 
+import java.util.List;
+
+import com.cg.Entity.Department;
+
 public interface DepartmentDao {
+
+ 
+
+	String addDepartment(Department d);
+
+ 
+
+	List<Department> getAllDepartment();
+
+ 
 
 }
