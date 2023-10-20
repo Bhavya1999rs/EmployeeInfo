4e29b286d3a4afd3879860960eea3d5003acbf2c
Bhavya1999rs <148413959+Bhavya1999rs@users.noreply.github.com>
2023-10-20 10:31:56 +0530

    Update DepartmentService.java

--- src/main/java/com/cg/Service/DepartmentService.java
+++ src/main/java/com/cg/Service/DepartmentService.java
@@ -1,5 +1,42 @@
 package com.cg.Service;
 
-public class DepartmentService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.cg.Dao.DepartmentDao;
+import com.cg.Entity.Department;
+import com.cg.repository.DepartmentRepository;
+
+@Service
+
+public class DepartmentService implements DepartmentDao {
+
+	@Autowired
+
+	DepartmentRepository dr;
+
+
+	@Override
+
+	public String addDepartment(Department d) {
+
+		dr.save(d);
+
+		return "Department added.....";
+
+	}
+
+	@Override
+
+	public List<Department> getAllDepartment() {
+
+		
+
+		return dr.findAll();
+
+	}
 }
+
